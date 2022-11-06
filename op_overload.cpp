d350e8253d6229bd15e11c14d7262a2205f567ce
AliiceK <112477235+AliiceK@users.noreply.github.com>
2022-11-06 20:00:49 +0200

    operator overload with specs

--- op_overload.cpp
+++ op_overload.cpp
@@ -8,22 +8,27 @@ class Stack {
         stack<int> stackk;
 
     public:
+        // the function push() will take a new value as a paramter and returns the stack with this new item
         void push(int x) {
             stackk.push(x);
         }
 
+        // the function pop() takes no parameters and will pop the item at the top of the stack
         void pop() {
             stackk.pop();
         }
 
+        // the function top() will take no paramters and will return the value at the top of the stack
         int top() {
             return stackk.top();
         }
-
+        
+        // the function empty() will take no parameters and will return true or false depending whether the stack is empty or not
         bool empty() {
             return stackk.empty();
         }
 
+        // the function operator+() will take as parameters another stack and will return a new stack
         Stack operator+ (Stack &b) {
             Stack temporarystack;
             while (!stackk.empty()) {
